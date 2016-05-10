0933c2aa455fd157c0a72592c510a8e43108f71e
dbasak <dbasak@bol.com>
2016-05-10 02:21:31 +0200

    Working tests
    Change-Id: I24d45657b6110ed418c239548707275e80ca41ef

--- Vagrantfile
+++ Vagrantfile
@@ -3,7 +3,7 @@ Vagrant.configure(2) do |config|
 
   config.vm.box = "ubuntu/trusty64"
 
-  config.vm.network "private_network", ip: "192.168.33.10"
+  config.vm.network "private_network", ip: "192.168.50.4"
 
   config.vm.synced_folder "mount", "/vagrant_data"
 
--- mount/spec/campaign_spec.rb
+++ mount/spec/campaign_spec.rb
@@ -4,16 +4,27 @@ require 'rspec'
 require 'minitest/autorun'
 require './app/campaign_manager'
 
-class CampaignManagerTest < Minitest::Test
-  def test_should_always_return_unique_banners
+class CampaignManagerTest < Minitest::Unit::TestCase
+
+  def setup
     @banners = []
 
     (0..9).each do
       banner = CampaignManager.banners_based_on_revenue(20, @banners)
       @banners = @banners.push(banner)
     end
+  end
 
+  def test_should_always_return_unique_banners
     assert_equal(@banners.size, 10)
+  end
+
+  def test_should_banners_in_revenue_order
+
+    @campaign1 = CampaignManager.findBestBanners(20,[])
+    @campaign2 = CampaignManager.findBestBanners(20,@campaign1)
+
+    assert(@campaign1[0].revenue >= @campaign2[0].revenue)
   end
 
 end
